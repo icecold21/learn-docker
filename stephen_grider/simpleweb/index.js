952e63c579b7aae424bb94aa93cad8a7b4e291a0
Jonathan Wylliem <jonathan.wylliem@jurnal.id>
2019-02-04 10:46:28 +0700

    Distribute build step

--- stephen_grider/simpleweb/index.js
+++ stephen_grider/simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('yo there');
+  res.send('HOW ARE YOU DOING?');
 });
 
 app.listen(8080, () => {
