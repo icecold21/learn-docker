027ca364a08453ee936407e426c1235f6382c091
Jonathan Wylliem <jonathan.wylliem@jurnal.id>
2019-02-06 14:16:45 +0700

    Add all files

--- stephen_grider/frontend/src/App.js
+++ stephen_grider/frontend/src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reloads..
-            Hi thereere.
+            I was changed on feature branch. Again
           </p>
           <a
             className="App-link"
